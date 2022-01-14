3a845f0e8f7b836043b82691cabdbdf4c761ecd8
Patrick Kabuga <kabugap02@gmail.com>
2022-01-14 18:09:02 +0300

    Edited the banner bg pic, added an auto-typing, added color variables

--- ext.js
+++ ext.js
@@ -11,7 +11,17 @@ function toggleMenu() {
     menu.classList.toggle("active");
 }
 
+//  $(window).load(function() {
 
+//     // will first fade out the loading animation 
+//       $("#loader").fadeOut("slow", function(){
+
+//       // will fade out the whole DIV that covers the website.
+//       $("#preloader").delay(300).fadeOut("slow");
+
+//     });       
+
+//     })
 
 
 
