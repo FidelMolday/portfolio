142792555c25bb1430a9d6b24952cae1abdaae64
Patrick Kabuga <kabugap02@gmail.com>
2022-01-15 15:07:56 +0300

    Added an external js file and edited the auto-typing

---
+++ typed.js
@@ -0,0 +1,18 @@
+let typed = new Typed(".auto-input", {
+    strings: [ "a FrontEnd Web Developer,", "a Freelancer,", "a Web Designer,", "a Computer Science Student."],
+    typeSpeed: 50,
+    backSpeed:30,
+    loop:true,
+    backDelay: 2100,
+    showCursor: true,
+    cursorChar: '|',
+});
+let typed2 = new Typed(".auto-text", {
+    strings: [ "a FrontEnd Web Developer,", "a Freelancer,", "a Web Designer,", "a Computer Science Student."],
+    typeSpeed: 50,
+    backSpeed:30,
+    loop:true,
+    backDelay: 2100,
+    showCursor: true,
+    cursorChar: '|',
+});
