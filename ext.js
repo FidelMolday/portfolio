25ac7717c0b4cfd69ae8489e6e26be65e4bb7c34
Patrick Kabuga <kabugap02@gmail.com>
2022-01-16 22:21:34 +0300

    Added a scroll to top button

--- ext.js
+++ ext.js
@@ -10,7 +10,20 @@ function toggleMenu() {
     menuToggle.classList.toggle("active");
     menu.classList.toggle("active");
 }
-
+// Scroll top button
+window.addEventListener("scroll", () => {
+    const scrollHeight = window.scrollY;
+    const moveTopButton = document.querySelector(".move-top");
+    if(scrollHeight > 520) {
+        moveTopButton.style.opacity = 1;
+        moveTopButton.style.top = "85vh";
+        moveTopButton.style.right = "5vw";
+    } else {
+        moveTopButton.style.opacity = 0;
+        moveTopButton.style.top = "50vh";
+        moveTopButton.style.right = "5vw";
+    }
+});
 //  $(window).load(function() {
 
 //     // will first fade out the loading animation 
