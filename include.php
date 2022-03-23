d83839532f37554f70d19b4f658effefede5fd4d
Patrick Kabuga <kabugap02@gmail.com>
2022-03-23 16:10:56 +0300

    Added a php file to kick off connection with a mySQL database

---
+++ include.php
@@ -0,0 +1,7 @@
+<?php
+$dbhost="sql206.epizy.com";
+$dbuser="epiz_31157008";
+$dbpass="qQ4VRvaEqG47nyO";
+$conn=mysql_connect($dbhost,$dbuser,$dbpass);
+mysql_select_db('epiz_31157008_Messages',$conn);
+?>
